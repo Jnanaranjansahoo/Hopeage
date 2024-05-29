8e6453463ff8af9b0530d390f58c85ad606d10b7
jnana <jnanaranjansahoo016@gmail.com>
2024-05-29 16:27:50 +0530

    a

--- HopeageWeb/Controllers/PhotoController.cs
+++ HopeageWeb/Controllers/PhotoController.cs
@@ -1,4 +1,5 @@
 ﻿using HopeageWeb.Data;
+using HopeageWeb.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HopeageWeb.Controllers
@@ -13,7 +14,90 @@ namespace HopeageWeb.Controllers
         }
         public IActionResult Index()
         {
+            List<Photo> objPhotoList = _db.Photos.ToList();
+            return View(objPhotoList);
+        }
+        public IActionResult Create()
+        {
+           
             return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Photo obj)
+        {
+            
+            if (ModelState.IsValid)
+            {
+                _db.Photos.Add(obj);
+                
+                _db.SaveChanges();
+                TempData["success"] = "Officer Created Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Photo? photoFromDb = _db.Photos.Find(id);
+
+            if (photoFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(photoFromDb);
+
+        }
+        [HttpPost]
+        public IActionResult Edit(Photo obj)
+        {
+            if (ModelState.IsValid)
+            {
+                
+                _db.Photos.Update(obj);
+                _db.SaveChanges();
+                TempData["success"] = "Officer Update Successfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+
+        }
+
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            Photo? photoFromDb = _db.Photos.Find(id);
+
+            if (photoFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(photoFromDb);
+
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? id)
+        {
+            Photo? obj = _db.Photos.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Photos.Remove(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Officer Update Successfully";
+            return RedirectToAction("Index");
+
         }
     }
 }
--- HopeageWeb/Models/Photo.cs
+++ HopeageWeb/Models/Photo.cs
@@ -6,7 +6,7 @@ namespace HopeageWeb.Models
     {
         [Key]
         public int Id { get; set; }
-
+        [Required]
         public string? Image { get; set; }
     }
 }
